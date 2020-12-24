97ce6ed06126f760000cce00e5ac2ebcc9a81340
wonAdam <ehdgus5500@gmail.com>
2020-12-24 21:51:14 +0900

    1110 cpp cs

---
+++ 1110_AdditionCycle/cs/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            int input = int.Parse(Console.ReadLine());
+
+            // if input less than 10
+            if(input < 10){
+                input *= 10;
+            }
+
+            //process
+            int count = 0;
+            int num = input;
+            while(true)
+            {
+                count++;
+
+                int newFirstDigit = ((num / 10) + (num % 10)) % 10;
+                int newSecondDigit = num % 10;
+
+                int newNum= newSecondDigit * 10 + newFirstDigit;
+
+                if(newNum == input)
+                    break;
+                else
+                    num = newNum;
+            }
+
+            // print result
+            Console.WriteLine(count);
+            
+
+            Console.Read();
+            return;
+        }
+    }
+}
