d85836d5325a39e93d3b12c954f4ae5d215d7e48
wonAdam <ehdgus5500@gmail.com>
2021-02-04 19:14:53 +0900

    Revert "Revert "LeetCode 1 solved""
    This reverts commit a0b19453bad9139ede5dbafb610393732df352b2.

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/LeetCode_NumberOf1Bits.cs
@@ -0,0 +1,13 @@
+﻿//using System;
+//using System.Linq;
+
+//namespace Baekjoon_CSharp
+//{
+//    class LeetCode_NumberOf1Bits
+//    {
+//        public int HammingWeight(uint n)
+//        {
+//            return Convert.ToString(n, 2).Count((c) => c == '1');
+//        }
+//    }
+//}
