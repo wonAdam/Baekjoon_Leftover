ae0f69164c9464260583ecab7b5829589c0fd657
AdamWon <ehdgus5500@gmail.com>
2021-02-08 19:48:30 +0900

    백준 2문제 백준 1767 최단경로 다익스트라로 했는데 시간초과..? 백준 1764 듣보잡 정렬로하는거였군.. 컽!

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/1753_ShortestPath.cs
@@ -0,0 +1,83 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+
+//namespace Baekjoon_CSharp
+//{
+//    class _1753_ShortestPath
+//    {
+//        public class Edge
+//        {
+//            public int from;
+//            public int to;
+//            public int cost;
+//            public Edge(int[] input)
+//            {
+//                this.from = input[0]-1;
+//                this.to = input[1]-1;
+//                this.cost = input[2];
+//            }
+//        }
+
+
+//        static void Main()
+//        {
+//            int[] ve = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToArray();
+//            int vertex = ve[0];
+//            int edge = ve[1];
+
+//            int start = int.Parse(Console.ReadLine());
+//            start -= 1;
+
+//            List<Edge> edges = new List<Edge>(); 
+//            for(int i = 0; i < edge; i++)
+//            {
+//                int[] input = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToArray();
+//                Edge e = new Edge(input);
+//                edges.Add(e);
+//            }
+
+//            int[] dist = new int[vertex]; 
+
+//            bool[] sptSet = new bool[vertex];
+
+//            for (int i = 0; i < vertex; i++)
+//            {
+//                dist[i] = int.MaxValue;
+//                sptSet[i] = false;
+//            }
+//            dist[start] = 0;
+
+//            for(int i = 0; i < vertex; i++)
+//            {
+//                int u = 0;
+//                int min = int.MaxValue;
+//                for(int j = 0; j < vertex; j++)
+//                {
+//                    if(min > dist[j] && !sptSet[j])
+//                    {
+//                        min = dist[j];
+//                        u = j;
+//                    }
+//                }
+
+//                sptSet[u] = true;
+
+//                for(int v = 0; v < vertex; v++)
+//                {
+//                    if (!sptSet[v] && edges.Any(e => e.from == u && e.to == v) && dist[u] != int.MaxValue &&
+//                        dist[u] + edges.Find(e => e.from == u && e.to == v).cost < dist[v])
+//                        dist[v] = dist[u] + edges.Find(e => e.from == u && e.to == v).cost;
+//                }
+//            }
+
+
+
+//            foreach(var d in dist)
+//            {
+//                string print = d == int.MaxValue ? "INF" : d.ToString();
+//                Console.WriteLine(print);
+//            }
+//        }
+//    }
+//}
--- Baekjoon_Cpp/Baekjoon_Cpp/l1_PracticeTest.cpp
+++ Baekjoon_Cpp/Baekjoon_Cpp/l1_PracticeTest.cpp
@@ -39,13 +39,13 @@
 //	return 0;
 //}
 
-#include <vector>
-#include <iostream>
-
-int main()
-{
-	std::vector<int> v = std::vector<int>(5, 3);
-
-	for (std::vector<int>::const_iterator iter = v.begin(); iter != v.end(); iter++)
-		std::cout << *iter << std::endl;
-}
+//#include <vector>
+//#include <iostream>
+//
+//int main()
+//{
+//	std::vector<int> v = std::vector<int>(5, 3);
+//
+//	for (std::vector<int>::const_iterator iter = v.begin(); iter != v.end(); iter++)
+//		std::cout << *iter << std::endl;
+//}
