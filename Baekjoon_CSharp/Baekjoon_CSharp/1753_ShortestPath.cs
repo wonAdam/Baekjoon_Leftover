7085c63dee8d694bbe684775693a92467d88a8ab
AdamWon <ehdgus5500@gmail.com>
2021-02-08 22:34:23 +0900

    도덕책...뭔데...

--- Baekjoon_CSharp/Baekjoon_CSharp/1753_ShortestPath.cs
+++ Baekjoon_CSharp/Baekjoon_CSharp/1753_ShortestPath.cs
@@ -1,83 +1,57 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
+﻿using System;
+using System.Linq;
 
-//namespace Baekjoon_CSharp
-//{
-//    class _1753_ShortestPath
-//    {
-//        public class Edge
-//        {
-//            public int from;
-//            public int to;
-//            public int cost;
-//            public Edge(int[] input)
-//            {
-//                this.from = input[0]-1;
-//                this.to = input[1]-1;
-//                this.cost = input[2];
-//            }
-//        }
+namespace Baekjoon_CSharp
+{
+    class _1753_ShortestPath
+    {
+        static void Main()
+        {
+            int[] ve = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToArray();
+            int vertex = ve[0];
+            int edge = ve[1];
 
+            int start = int.Parse(Console.ReadLine());
+            start -= 1;
 
-//        static void Main()
-//        {
-//            int[] ve = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToArray();
-//            int vertex = ve[0];
-//            int edge = ve[1];
+            byte[,] graph = new byte[vertex, vertex];
+            for (int i = 0; i < edge; i++)
+            {
+                int[] input = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToArray();
+                graph[input[0] - 1, input[1] - 1] = (byte)input[2];
+            }
 
-//            int start = int.Parse(Console.ReadLine());
-//            start -= 1;
+            int[] dist = new int[vertex];
 
-//            List<Edge> edges = new List<Edge>(); 
-//            for(int i = 0; i < edge; i++)
-//            {
-//                int[] input = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToArray();
-//                Edge e = new Edge(input);
-//                edges.Add(e);
-//            }
+            bool[] sptSet = new bool[vertex];
+            for (int i = 0; i < vertex; i++)
+            {
+                dist[i] = int.MaxValue;
+                sptSet[i] = false;
+            }
+            dist[start] = 0;
 
-//            int[] dist = new int[vertex]; 
+            for (int i = 0; i < vertex; i++)
+            {
+                int u = Array.IndexOf(dist, dist.Where((d, i) => !sptSet[i]).Min());
 
-//            bool[] sptSet = new bool[vertex];
+                sptSet[u] = true;
 
-//            for (int i = 0; i < vertex; i++)
-//            {
-//                dist[i] = int.MaxValue;
-//                sptSet[i] = false;
-//            }
-//            dist[start] = 0;
-
-//            for(int i = 0; i < vertex; i++)
-//            {
-//                int u = 0;
-//                int min = int.MaxValue;
-//                for(int j = 0; j < vertex; j++)
-//                {
-//                    if(min > dist[j] && !sptSet[j])
-//                    {
-//                        min = dist[j];
-//                        u = j;
-//                    }
-//                }
-
-//                sptSet[u] = true;
-
-//                for(int v = 0; v < vertex; v++)
-//                {
-//                    if (!sptSet[v] && edges.Any(e => e.from == u && e.to == v) && dist[u] != int.MaxValue &&
-//                        dist[u] + edges.Find(e => e.from == u && e.to == v).cost < dist[v])
-//                        dist[v] = dist[u] + edges.Find(e => e.from == u && e.to == v).cost;
-//                }
-//            }
+                foreach (var item in Enumerable.Range(0, vertex).Select(x => new { to = x, cost = graph[u, x] }).Where(c => c.cost > 0))
+                {
+                    if (!sptSet[item.to] && dist[u] != int.MaxValue &&
+                        dist[u] + item.cost < dist[item.to])
+                        dist[item.to] = dist[u] + item.cost;
+                }
+            }
 
 
 
-//            foreach(var d in dist)
-//            {
-//                string print = d == int.MaxValue ? "INF" : d.ToString();
-//                Console.WriteLine(print);
-//            }
-//        }
-//    }
-//}
+            foreach (var d in dist)
+            {
+                string print = d == int.MaxValue ? "INF" : d.ToString();
+                Console.WriteLine(print);
+            }
+        }
+    }
+}
--- Baekjoon_CSharp/Baekjoon_CSharp/1764_NotThatIveHeardOrKnown.cs
+++ Baekjoon_CSharp/Baekjoon_CSharp/1764_NotThatIveHeardOrKnown.cs
@@ -1,33 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
 
-namespace Baekjoon_CSharp
-{
-    class _1764_NotThatIveHeardOrKnown
-    {
-        static  void Main()
-        {
-            int[] nm = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-            int n = nm[0];
-            int m = nm[1];
+//namespace Baekjoon_CSharp
+//{
+//    class _1764_NotThatIveHeardOrKnown
+//    {
+//        static  void Main()
+//        {
+//            int[] nm = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+//            int n = nm[0];
+//            int m = nm[1];
 
-            List<string> nList = new List<string>();
-            List<string> answer = new List<string>();
-            for(int i = 0; i < n+m; i++)
-            {
-                nList.Add(Console.ReadLine());
-            }
-            nList.Sort();
-            for (int i = 0; i < n+m-1; i++)
-            {
-                if (nList[i] == nList[i + 1]) answer.Add(nList[i]);
-            }
+//            List<string> nList = new List<string>();
+//            List<string> answer = new List<string>();
+//            for(int i = 0; i < n+m; i++)
+//            {
+//                nList.Add(Console.ReadLine());
+//            }
+//            nList.Sort();
+//            for (int i = 0; i < n+m-1; i++)
+//            {
+//                if (nList[i] == nList[i + 1]) answer.Add(nList[i]);
+//            }
 
-            Console.WriteLine(answer.Count);
-            foreach(var name in answer)
-                Console.WriteLine(name);
+//            Console.WriteLine(answer.Count);
+//            foreach(var name in answer)
+//                Console.WriteLine(name);
                 
-        }
-    }
-}
+//        }
+//    }
+//}
