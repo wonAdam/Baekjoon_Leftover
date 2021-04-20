b7fedce185ee54ff832d5c39fa63eae77deef33d
AdamWon <ehdgus5500@gmail.com>
2021-04-20 18:26:31 +0900

    백준 10825 : 국영수 1. 시간초과 : Linq의 한계..? 2. But Linq의 문제가 아니라 출력을 StringBuilder로 해주니 해결 3. 출력이 많을때 한 번 호출의 Console.WriteLine으로 바꿔주는것이 차이가 많이 나는 듯.

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/10825_kook-young-soo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Baekjoon_CSharp
+{
+    struct Student
+    {
+        public string _name;
+        public int _kor;
+        public int _eng;
+        public int _math;
+        public Student(string name, int k, int e, int m)
+        {
+            _name = name;
+            _kor = k; _eng = e; _math = m;
+        }
+    }
+
+    class _10825_kook_young_soo
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+            for(int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                int k = int.Parse(input[1]);
+                int e = int.Parse(input[2]);
+                int m = int.Parse(input[3]);
+
+                students.Add(new Student(name, k, e, m));
+            }
+
+            var orderedStudents = 
+                students
+                .OrderByDescending(s => s._kor)
+                .ThenBy(s => s._eng)
+                .ThenByDescending(s => s._math)
+                .ThenBy(s => s._name, StringComparer.Ordinal);
+
+            StringBuilder sb = new StringBuilder();
+
+
+            foreach (var s in orderedStudents)
+                sb.AppendLine(s._name);
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
