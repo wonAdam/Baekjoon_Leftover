07356d711c54976105083d1794e35ecc69f754df
원동현 <ehdgus5500@gmail.com>
2021-09-21 20:58:27 +0900

    LeetCode 190. Reverse Bits

---
+++ Baekjoon_Cpp/Baekjoon_Cpp/leetcode_190.cpp
@@ -0,0 +1,28 @@
+//#include <cstdint>
+//#include <bitset>
+//#include <sstream>
+//
+//using namespace std;
+//
+//class Solution {
+//public:
+//    uint32_t reverseBits(uint32_t n) {
+//        bitset<32> b(n);
+//
+//        string bit_string = b.to_string();
+//        stringstream ss;
+//        for (int i = bit_string.size() - 1; i >= 0; --i)
+//            ss << bit_string[i];
+//
+//        bitset<32> result(ss.str());
+//        return result.to_ulong();
+//    }
+//};
+//
+//int main()
+//{
+//    Solution sol;
+//    uint32_t result = sol.reverseBits(0b00000010100101000001111010011100);
+//
+//    return 0;
+//}
--- Baekjoon_Cpp/Baekjoon_Cpp/leetcode_20.cpp
+++ Baekjoon_Cpp/Baekjoon_Cpp/leetcode_20.cpp
@@ -1,64 +1,64 @@
-#include <string>
-#include <stack>
-
-using namespace std;
-
-class Solution {
-public:
-    bool isValid(const string& s) {
-        stack<char> stack;
-
-        for (const char& c : s)
-        {
-            switch (c)
-            {
-            case '}':
-            {
-                if (stack.size() > 0 && stack.top() == '{')
-                    stack.pop();
-                else
-                    return false;
-
-                break;
-            }
-            case ')':
-            {
-                if (stack.size() > 0 && stack.top() == '(')
-                    stack.pop();
-                else
-                    return false;
-
-                break;
-            }
-            case ']':
-            {
-                if (stack.size() > 0 && stack.top() == '[')
-                    stack.pop();
-                else
-                    return false;
-
-                break;
-            }
-            case '{':
-            case '(':
-            case '[':
-            {
-                stack.push(c);
-                break;
-            }
-            default:
-                break;
-            }
-        }
-
-        return stack.size() == 0;
-    }
-};
-
-
-int main()
-{
-    Solution sol;
-    sol.isValid("{}");
-    return 0;
-}
+//#include <string>
+//#include <stack>
+//
+//using namespace std;
+//
+//class Solution {
+//public:
+//    bool isValid(const string& s) {
+//        stack<char> stack;
+//
+//        for (const char& c : s)
+//        {
+//            switch (c)
+//            {
+//            case '}':
+//            {
+//                if (stack.size() > 0 && stack.top() == '{')
+//                    stack.pop();
+//                else
+//                    return false;
+//
+//                break;
+//            }
+//            case ')':
+//            {
+//                if (stack.size() > 0 && stack.top() == '(')
+//                    stack.pop();
+//                else
+//                    return false;
+//
+//                break;
+//            }
+//            case ']':
+//            {
+//                if (stack.size() > 0 && stack.top() == '[')
+//                    stack.pop();
+//                else
+//                    return false;
+//
+//                break;
+//            }
+//            case '{':
+//            case '(':
+//            case '[':
+//            {
+//                stack.push(c);
+//                break;
+//            }
+//            default:
+//                break;
+//            }
+//        }
+//
+//        return stack.size() == 0;
+//    }
+//};
+//
+//
+//int main()
+//{
+//    Solution sol;
+//    sol.isValid("{}");
+//    return 0;
+//}
