5b4ff0edf6b1d5eb09a420fa27f890c0c860dbe1
원동현 <ehdgus5500@gmail.com>
2021-09-21 22:08:48 +0900

    LeetCode 1791. Find Center of Star Graph

---
+++ Baekjoon_Cpp/Baekjoon_Cpp/leetcode_1791.cpp
@@ -0,0 +1,31 @@
+//#include <vector>
+//
+//using namespace std;
+//
+//class Solution {
+//public:
+//    int findCenter(vector<vector<int>>& edges) {
+//        vector<bool> counted(edges.size(), false);
+//
+//        for (const vector<int>& edge : edges)
+//        {
+//            if (counted[edge[0]] == true)
+//                return edge[0];
+//            else
+//                counted[edge[0]] = true;
+//
+//            if (counted[edge[1]] == true)
+//                return edge[1];
+//            else
+//                counted[edge[1]] = true;
+//        }
+//
+//        return -1;
+//    }
+//};
+//
+//int main()
+//{
+//
+//    return 0;
+//}
