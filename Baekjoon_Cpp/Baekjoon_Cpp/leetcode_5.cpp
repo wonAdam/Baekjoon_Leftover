14e6af5bac639ed60b348272bace23110f3f5f42
원동현 <ehdgus5500@gmail.com>
2021-09-22 21:11:00 +0900

    LeetCode 5. Longest Palindromic Substring

---
+++ Baekjoon_Cpp/Baekjoon_Cpp/leetcode_5.cpp
@@ -0,0 +1,48 @@
+﻿// https://leetcode.com/problems/longest-palindromic-substring/
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPanlindrome(const string& s, int start, int len)
+    {
+        for (int i = start, j = start + len - 1; i < j; ++i, --j)
+        {
+            if (s[i] != s[j])
+                return false;
+        }
+
+        return true;
+    }
+
+    string longestPalindrome(const string& s) 
+    {
+        int startIndex = 0, len = 0;
+        for (int i = 0; i < s.size(); ++i)
+        {
+            for (int j = s.size() - i; j > 0; --j)
+            {
+                if (len < j && isPanlindrome(s, i, j))
+                {
+                    startIndex = i;
+                    len = j;
+                }
+            }
+        }
+
+        return s.substr(startIndex, len);
+    }
+};
+
+int main()
+{
+    Solution sol;
+    string pal = sol.longestPalindrome("aba");
+    pal = sol.longestPalindrome("a");
+
+    // string의 복사가 일어나서 time exceed. substr를 loop마다쓰는 것이 원인이었음
+    pal = sol.longestPalindrome("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+    return 0;
+}
