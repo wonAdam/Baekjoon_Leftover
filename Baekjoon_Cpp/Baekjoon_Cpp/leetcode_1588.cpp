293cc621833eb2cb52672522f37be0778e3e71cc
원동현 <ehdgus5500@gmail.com>
2021-10-21 22:43:44 +0900

    LeetCode 1588. Sum of All Odd Length Subarrays

---
+++ Baekjoon_Cpp/Baekjoon_Cpp/leetcode_1588.cpp
@@ -0,0 +1,33 @@
+﻿//// https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+//
+//#include <vector>
+//
+//using namespace std;
+//
+//class Solution {
+//public:
+//    int sumOddLengthSubarrays(vector<int>& arr) {
+//        int accum = 0;
+//        vector<int> mult(arr.size(), 0);
+//        for (int i = 0; i < arr.size(); i++)
+//        {
+//            // 각 i부터 j까지의 합 중 길이가 홀수인 sum만 accum
+//            int sum = 0;
+//            for (int j = i; j < arr.size(); j++) 
+//            {
+//                sum += arr[j];
+//                if ((j - i + 1) % 2 != 0)
+//                    accum += sum;
+//            }
+//        }
+//
+//        return accum;
+//    }
+//};
+//
+//int main()
+//{
+//    vector<int> arr{ 1,4,2,5,3 };
+//    int result = Solution().sumOddLengthSubarrays(arr);
+//    return 0;
+//}
