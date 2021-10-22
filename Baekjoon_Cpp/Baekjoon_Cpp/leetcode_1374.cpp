be23e3bc7e832d7e447597082608a58e11b8819c
원동현 <ehdgus5500@gmail.com>
2021-10-22 10:55:36 +0900

    LeetCode 1374. Generate a String With Characters That Have Odd Counts

---
+++ Baekjoon_Cpp/Baekjoon_Cpp/leetcode_1374.cpp
@@ -0,0 +1,25 @@
+﻿//// https://leetcode.com/problems/generate-a-string-with-characters-that-have-odd-counts/
+//
+//#include <string>
+//
+//using namespace std;
+//
+//class Solution {
+//public:
+//    string generateTheString(int n) {
+//        if (n % 2 == 0) // 짝수
+//        {
+//            return string(n - 1, 'a') + 'b';
+//        }
+//        else // 홀수
+//        {
+//            return string(n, 'a');
+//        }
+//    }
+//};
+//
+//int main()
+//{
+//    string result = Solution().generateTheString(4);
+//    return 0;
+//}
