ced9d37d94facaeab30ae75741ed5b43cebb9ecd
원동현 <ehdgus5500@gmail.com>
2021-12-04 10:01:30 +0900

    LeetCode 405. Convert a Number to Hexadecimal

---
+++ Baekjoon_Cpp/Baekjoon_Cpp/leetcode_405.cpp
@@ -0,0 +1,23 @@
+//// https://leetcode.com/problems/convert-a-number-to-hexadecimal/
+//
+//#include <string>
+//#include <sstream>
+//
+//using namespace std;
+//
+//class Solution {
+//public:
+//    string toHex(int num) {
+//        stringstream ss;
+//        ss << std::hex << num;
+//        string res(ss.str());
+//
+//        return res;
+//    }
+//};
+//
+//int main()
+//{
+//    string result = Solution().toHex(-1);
+//    return 0;
+//}
