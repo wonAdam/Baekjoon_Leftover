e9e6c71bbf4208313686e77108221be49024d6fc
원동현 <ehdgus5500@gmail.com>
2023-01-31 23:36:18 +0900

    백준 1929번

---
+++ Baekjoon_Cpp/Baekjoon_Cpp/_1929.cpp
@@ -0,0 +1,36 @@
+#define baekjoon1929 1
+#if baekjoon1929
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	int min; int max;
+	std::cin >> min >> max;
+
+	std::vector<bool> primeNumbers;
+	for (int i = 0; i <= max; ++i)
+		primeNumbers.emplace_back(true);
+
+	// 1 is not a prime number.
+	primeNumbers[1] = false;
+
+	// Eratos
+	for (int i = 2; i * i <= max; ++i)
+	{
+		if (primeNumbers[i] == true)
+		{
+			for (int j = 2; j * i <= max; ++j)
+				primeNumbers[j * i] = false;
+		}
+	}
+
+	for (int i = min; i <= max; ++i)
+		if (primeNumbers[i] == true)
+			std::cout << i << '\n';
+
+	return 0;
+}
+
+#endif
