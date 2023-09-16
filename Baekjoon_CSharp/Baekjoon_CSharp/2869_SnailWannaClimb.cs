6857d88d05b2168d335247115f5b9c2d40bfb411
원동현 <ehdgus5500@gmail.com>
2023-09-16 18:42:15 +0900

    백준 2869

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/2869_SnailWannaClimb.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _2869_SnailWannaClimb
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<int> input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+//            int a = input[0];
+//            int b = input[1];
+//            int v = input[2];
+
+//            int div = (v - b) / (a - b);
+//            int remain = (v - b) % (a - b);
+
+//            Console.WriteLine(remain > 0 ? div + 1 : div);
+//        }
+//    }
+//}
