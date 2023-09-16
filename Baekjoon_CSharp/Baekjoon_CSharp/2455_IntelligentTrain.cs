2f3d0c2e80853cfa5f3e33369300c0dc7d83cac1
원동현 <ehdgus5500@gmail.com>
2023-09-16 18:32:42 +0900

    백준 2455

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/2455_IntelligentTrain.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _2455_IntelligentTrain
+//    {
+//        static void Main(string[] args)
+//        {
+//            int curr = 0;
+//            int max = 0;
+//            for(int i = 0; i < 4; i++)
+//            {
+//                List<int> input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+//                int getOff = input[0];
+//                int getOn = input[1];
+
+//                curr = Math.Min(curr + (getOn - getOff), 10000);
+//                max = Math.Max(max, curr);
+//            }
+
+//            Console.WriteLine( max );
+//        }
+//    }
+//}
