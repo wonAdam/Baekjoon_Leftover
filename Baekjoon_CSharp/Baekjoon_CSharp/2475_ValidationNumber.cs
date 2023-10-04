fb1f0303f6375a8986c608d1aab80b49f4fe882d
원동현 <ehdgus5500@gmail.com>
2023-10-04 21:10:14 +0900

    231004 leeyebin

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/2475_ValidationNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Baekjoon_CSharp
+{
+    internal class _2475_ValidationNumber
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] inputArr = input.Split(' ');
+
+            int accum = 0;
+            foreach(var inputElem in inputArr) 
+            { 
+                int inputElemInt = int.Parse(inputElem);
+                int pow = inputElemInt * inputElemInt;
+                accum += pow; 
+            }
+
+            Console.WriteLine(accum % 10);
+        }
+    }
+}
