cb64132aa67363edb2e4a85613e398f1a1023b22
원동현 <ehdgus5500@gmail.com>
2023-10-06 20:36:16 +0900

    백준 10926, 18108

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/10926_HmHmWhat.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _10926_HmHmWhat
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine($"{Console.ReadLine()}??!");
+//        }
+//    }
+//}
---
+++ Baekjoon_CSharp/Baekjoon_CSharp/18108_MyAgeInThailand.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _18108_MyAgeInThailand
+//    {
+//        static void Main(string[] args)
+//        {
+//            int diff = 1998 - 2541;
+//            int input = int.Parse(Console.ReadLine());
+
+//            Console.WriteLine(input + diff);
+//        }
+//    }
+//}
