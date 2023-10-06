12e3b7e87e1fea2595cbb958244dea523298ddef
원동현 <ehdgus5500@gmail.com>
2023-10-06 20:37:19 +0900

    comment it out

--- Baekjoon_CSharp/Baekjoon_CSharp/2475_ValidationNumber.cs
+++ Baekjoon_CSharp/Baekjoon_CSharp/2475_ValidationNumber.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Runtime.InteropServices;
+//using System.Text;
 
-namespace Baekjoon_CSharp
-{
-    internal class _2475_ValidationNumber
-    {
-        static void Main(string[] args)
-        {
-            string input = Console.ReadLine();
-            string[] inputArr = input.Split(' ');
+//namespace Baekjoon_CSharp
+//{
+//    internal class _2475_ValidationNumber
+//    {
+//        static void Main(string[] args)
+//        {
+//            string input = Console.ReadLine();
+//            string[] inputArr = input.Split(' ');
 
-            int accum = 0;
-            foreach(var inputElem in inputArr) 
-            { 
-                int inputElemInt = int.Parse(inputElem);
-                int pow = inputElemInt * inputElemInt;
-                accum += pow; 
-            }
+//            int accum = 0;
+//            foreach(var inputElem in inputArr) 
+//            { 
+//                int inputElemInt = int.Parse(inputElem);
+//                int pow = inputElemInt * inputElemInt;
+//                accum += pow; 
+//            }
 
-            Console.WriteLine(accum % 10);
-        }
-    }
-}
+//            Console.WriteLine(accum % 10);
+//        }
+//    }
+//}
