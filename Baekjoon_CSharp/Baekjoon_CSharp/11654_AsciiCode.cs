5cf74b3b9d6a8cee0b1ccf4e8e7343a2d8d82527
원동현 <ehdgus5500@gmail.com>
2023-10-07 12:06:57 +0900

    백준 11654, 3003

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/11654_AsciiCode.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _11654_AsciiCode
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine(Console.Read());
+//        }
+//    }
+//}
---
+++ Baekjoon_CSharp/Baekjoon_CSharp/3003_KingQueenRookBishop.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _3003_KingQueenRookBishop
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<int> desired = new List<int>() { 1, 1, 2, 2, 2, 8 };
+//            List<int> inputInt = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+//            for(int i = 0; i < desired.Count; i++) 
+//            {
+//                desired[i] -= inputInt[i];
+//            }
+
+//            Console.WriteLine(String.Join(' ', desired));
+//        }
+//    }
+//}
