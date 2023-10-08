7edfae5b6cd780e5a71ad3866871f890f2a67571
원동현 <ehdgus5500@gmail.com>
2023-10-08 18:55:50 +0900

    백준 2439

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/2439_PrintStar2.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _2439_PrintStar2
+//    {
+//        static void Main(string[] args)
+//        {
+//            int n = int.Parse(Console.ReadLine());
+
+//            StringBuilder sb = new StringBuilder();
+//            for (int i = 0; i < n; i++)
+//                sb.Append(' ');
+
+//            for (int i = n; i > 0; i--)
+//            {
+//                sb.Remove(i - 1, 1);
+//                sb.Insert(i - 1, '*');
+//                Console.WriteLine(sb.ToString());
+//            }
+//        }
+//    }
+//}
