e108f53abf3734ff562cc07730530e12e9578089
원동현 <ehdgus5500@gmail.com>
2023-10-14 16:01:23 +0900

    백준 15964

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/15964_StrangeSymbol.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _15964_StrangeSymbol
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<long> inputList = Console.ReadLine().Split().Select(long.Parse).ToList();
+
+//            long a = inputList[0];
+//            long b = inputList[1];
+
+//            Console.WriteLine((a + b) * (a - b));
+//        }
+//    }
+//}
