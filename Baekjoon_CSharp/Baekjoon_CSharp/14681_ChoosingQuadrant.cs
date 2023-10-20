c55155b8ad500e474636b8181d06616930b3d871
원동현 <ehdgus5500@gmail.com>
2023-10-20 18:16:49 +0900

    백준 14681

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/14681_ChoosingQuadrant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baekjoon_CSharp
+{
+    internal class _14681_ChoosingQuadrant
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if(x > 0 && y > 0) { Console.WriteLine(1); }
+            else if(x < 0 && y > 0) { Console.WriteLine(2); }
+            else if(x < 0 && y < 0) { Console.WriteLine(3); }
+            else if (x > 0 && y < 0) { Console.WriteLine(4); }
+        }
+    }
+}
