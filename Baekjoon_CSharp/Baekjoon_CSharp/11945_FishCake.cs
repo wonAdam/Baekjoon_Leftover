fb6f0dd10c94b5e6e5668ea153ec324d6446c0a6
원동현 <ehdgus5500@gmail.com>
2023-10-20 19:01:37 +0900

    백준 5597 & 11945

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/11945_FishCake.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _11945_FishCake
+//    {
+//        static void Main(string[] args)
+//        {
+//            int lineNum = Console.ReadLine().Split().Select(int.Parse).ToList()[0];
+//            for(int i = 0; i < lineNum; i++)
+//            {
+//                Console.WriteLine(String.Join("", Console.ReadLine().Reverse()));
+//            }
+//        }
+//    }
+//}
---
+++ Baekjoon_CSharp/Baekjoon_CSharp/5597_SomeoneDidntSubmitAssignment.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Text.Encodings.Web;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _5597_SomeoneDidntSubmitAssignment
+//    {
+//        static void Main(string[] args)
+//        {
+//            SortedSet<int> numbers = new SortedSet<int>();
+//            for(int i = 1; i <= 30; i++)
+//                numbers.Add(i);
+
+//            for (int i = 1; i <= 28; i++)
+//            {
+//                int input = int.Parse(Console.ReadLine());
+//                numbers.Remove(input);
+//            }
+
+//            foreach (int number in numbers)
+//                Console.WriteLine(number);
+//        }
+//    }
+//}
