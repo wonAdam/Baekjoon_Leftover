4707dc6094e977eb6421f016aa3432c9239ae6a9
원동현 <ehdgus5500@gmail.com>
2023-11-01 23:27:44 +0900

    백준 10250

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/10250_ACMHotel.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _10250_ACMHotel
+//    {
+//        static void Main(string[] args)
+//        {
+//            int t = int.Parse(Console.ReadLine());
+
+//            for(int i = 0; i < t; i++)
+//            {
+//                var inputList = Console.ReadLine().Split().Select(int.Parse).ToList();
+//                int h = inputList[0];
+//                int w = inputList[1];
+//                int m = inputList[2];
+
+//                int floor = m % h;
+//                int number = (m - 1) / h + 1;
+
+//                string floorPart = floor == 0 ? h.ToString() : floor.ToString(); 
+//                string numberPart = number < 10 ? "0" + number.ToString() : number.ToString(); 
+//                Console.WriteLine($"{floorPart}{numberPart}");
+//            }
+//        }
+//    }
+//}
