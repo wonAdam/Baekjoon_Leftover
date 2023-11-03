81164809c8418b32b1c17456b4c856d48cea74e8
원동현 <ehdgus5500@gmail.com>
2023-11-03 23:55:48 +0900

    백준 2914

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/2914_FAKTOR.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _2914_FAKTOR
+//    {
+//        static void Main(string[] arg)
+//        {
+//            var inputList = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+//            int a = inputList[0];
+//            int i = inputList[1];
+
+//            Console.WriteLine(a * (i - 1) + 1);
+//        }
+//    }
+//}
