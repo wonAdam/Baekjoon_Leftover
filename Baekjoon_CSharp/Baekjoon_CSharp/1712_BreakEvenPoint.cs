d19571e3f475f9c0518529058ddad49afde2d34d
원동현 <ehdgus5500@gmail.com>
2023-11-04 10:37:08 +0900

    백준 두문제

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/1712_BreakEvenPoint.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _1712_BreakEvenPoint
+//    {
+//        static void Main(string[] args)
+//        {
+//            var inputList = Console.ReadLine().Split().Select(int.Parse).ToList();
+//            int a = inputList[0];
+//            int b = inputList[1];
+//            int c = inputList[2];
+
+//            if(b >= c)
+//            {
+//                Console.WriteLine(-1);
+//            }
+//            else
+//            {
+//                Console.WriteLine((a / (c - b)) + 1);
+//            }
+//        }
+//    }
+//}
---
+++ Baekjoon_CSharp/Baekjoon_CSharp/1978_FindingPrimeNumbers.cs
@@ -0,0 +1,42 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _1978_FindingPrimeNumbers
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.ReadLine();
+
+//            var inputList = Console.ReadLine().Split().Select(int.Parse).ToList();
+//            inputList.Sort();
+
+//            int PrimeNumberCount = 0;
+//            foreach(var input in inputList)
+//            {
+//                if (input == 1)
+//                    continue;
+
+//                bool bIsPrimeNumber = true;
+//                for(int i = 2; i < input; i++)
+//                {
+//                    if(input % i == 0)
+//                    {
+//                        bIsPrimeNumber = false;
+//                        break;
+//                    }
+//                }
+
+//                if(bIsPrimeNumber == true)
+//                {
+//                    PrimeNumberCount++;
+//                }
+//            }
+
+//            Console.WriteLine(PrimeNumberCount);
+//        }
+//    }
+//}
