1527f757357c320f69f9b5553fe12214c5a8292c
원동현 <ehdgus5500@gmail.com>
2023-11-11 16:12:30 +0900

    백준 FBI, Olympic은 풀다가맘

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/2857_FBI.cs
@@ -0,0 +1,55 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _2857_FBI
+//    {
+//        static void Main(string[] args)
+//        {
+//            //List<string> list = new List<string>(); 
+//            //for(int i = 0; i < 5; i++)
+//            //{
+//            //    string input = Console.ReadLine();
+//            //    list.Add(input);
+//            //}
+
+//            //foreach(string input in list) 
+//            //{
+            
+//            //}
+
+//            //string a = Console.ReadLine();
+//            //string b = Console.ReadLine();
+//            //string c = Console.ReadLine();
+//            //string d = Console.ReadLine();
+//            //string e = Console.ReadLine();
+
+//            //List<int> fbi = new List<int>();
+//            //if(a.Contains("FBI"))
+//            //{
+//            //    fbi.Add(1);
+//            //}
+//            //if (b.Contains("FBI"))
+//            //{
+//            //    fbi.Add(2);
+//            //}
+//            //if (c.Contains("FBI"))
+//            //{
+//            //    fbi.Add(3);
+//            //}
+//            //if (d.Contains("FBI"))
+//            //{
+//            //    fbi.Add(4);
+//            //}
+//            //if (e.Contains("FBI"))
+//            //{
+//            //    fbi.Add(5);
+//            //}
+
+
+
+//        }
+//    }
+//}
---
+++ Baekjoon_CSharp/Baekjoon_CSharp/8979_Olympic.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Baekjoon_CSharp
+{
+    internal class _8979_Olympic
+    {
+        struct Record
+        {
+            public int country;
+            public int gold;
+            public int silver;
+            public int bronze;
+        }
+
+
+        static void Main(string[] arg)
+        {
+            var nm = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int n = nm[0];
+            int m = nm[1];
+
+            List<Record> records = new List<Record>();
+            for(int i = 0; i < n; i++) 
+            {
+                var cgsb = Console.ReadLine().Split().Select(int.Parse).ToList();
+                int country = cgsb[0];
+                int gold = cgsb[1];
+                int silver = cgsb[2];
+                int bronze = cgsb[3];
+
+                Record record = new Record();
+                record.country = country;
+                record.gold = gold;
+                record.silver = silver;
+                record.bronze = bronze;
+                records.Add(record);
+            }
+
+            records.Sort((record1, record2) =>
+            {
+                if(record1.gold != record2.gold) return record2.gold - record1.gold;
+
+                if (record1.silver != record2.silver) return record2.silver - record1.silver;
+
+                if (record1.bronze != record2.bronze) return record2.bronze - record1.bronze;
+
+                return 0;
+            });
+
+
+            for(int i = 0; i <  records.Count; i++) 
+            {
+                if(records[i].country == m)
+                {
+                    Console.WriteLine(i + 1);
+                    break;
+                }
+            }
+
+        }
+    }
+}
