b2e86dbb9c7bf755c6a33fd637a4a956a88197f0
원동현 <ehdgus5500@gmail.com>
2023-11-20 22:09:17 +0900

    백준 3460번

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/3460_BinaryNumber.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _3460_BinaryNumber
+//    {
+//        static void Main(string[] args)
+//        {
+//            int t = int.Parse(Console.ReadLine());
+
+//            for(int i = 0; i < t; i++)
+//            {
+//                int n = int.Parse(Console.ReadLine());
+//                List<int> list = new List<int>();
+//                for(int j = 0; n > 0; j++)
+//                {
+//                    if(n % 2 != 0)
+//                        list.Add(j);
+
+//                    n = n >> 1;
+//                }
+
+//                Console.WriteLine(String.Join(" ", list));
+//            }
+//        }
+//    }
+//}
