50cf53c42b2e989090d84d0eb6a6f25bb60b379f
원동현 <ehdgus5500@gmail.com>
2023-12-03 11:59:20 +0900

    백준 2506번

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/2506_CalcScore.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _2506_CalcScore
+//    {
+//        static void Main(string[] args)
+//        {
+//            // 첫 입력 버리기
+//            Console.ReadLine();
+
+//            List<int> intInputList = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+//            int score = 0;
+//            int accum = 0;
+//            foreach(int intInput in intInputList)
+//            {
+//                if(intInput == 1)
+//                {
+//                    accum++;
+//                }
+//                else // 0
+//                {
+//                    accum = 0;
+//                }
+
+//                score += accum;
+//            }
+
+//            Console.WriteLine(score);
+//        }
+//    }
+//}
