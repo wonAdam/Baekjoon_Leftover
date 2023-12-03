ad7844e99c80a24d89d630a25eed6ed77980dd89
원동현 <ehdgus5500@gmail.com>
2023-12-03 14:38:59 +0900

    백준 8958번

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/8958_OXQuiz.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _8958_OXQuiz
+//    {
+//        static void Main(string[] args)
+//        {
+//            int n = int.Parse(Console.ReadLine());
+
+//            for(int i = 0; i < n; i++)
+//            {
+//                string inputString = Console.ReadLine();
+
+//                int score = 0;
+//                int accum = 0;
+//                foreach(char inputChar in inputString)
+//                {
+//                    if(inputChar == 'O')
+//                    {
+//                        accum++;
+//                    }
+//                    else if(inputChar == 'X')
+//                    {
+//                        accum = 0;
+//                    }
+//                    score += accum;
+//                }
+
+//                Console.WriteLine(score);
+//            }
+//        }
+//    }
+//}
