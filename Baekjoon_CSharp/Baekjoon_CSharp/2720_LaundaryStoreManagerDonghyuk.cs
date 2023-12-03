046339f5d940b0e36fee35516f740cb0fc772527
원동현 <ehdgus5500@gmail.com>
2023-12-03 16:20:05 +0900

    Update 2720_LaundaryStoreManagerDonghyuk.cs

--- Baekjoon_CSharp/Baekjoon_CSharp/2720_LaundaryStoreManagerDonghyuk.cs
+++ Baekjoon_CSharp/Baekjoon_CSharp/2720_LaundaryStoreManagerDonghyuk.cs
@@ -1,36 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace Baekjoon_CSharp
-{
-    internal class _2720_LaundaryStoreManagerDonghyuk
-    {
-        static void Main(string[] arg)
-        {
-            int t = int.Parse(Console.ReadLine());
+//namespace Baekjoon_CSharp
+//{
+//    internal class _2720_LaundaryStoreManagerDonghyuk
+//    {
+//        static void Main(string[] arg)
+//        {
+//            int t = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < t; i++)
-            {
-                int money = int.Parse(Console.ReadLine());
+//            for (int i = 0; i < t; i++)
+//            {
+//                int money = int.Parse(Console.ReadLine());
 
-                //25센트 = 1쿼터
-                int quarter = money / 25;
-                money %= 25;
+//                //25센트 = 1쿼터
+//                int quarter = money / 25;
+//                money %= 25;
 
-                //10센트 = 1다임
-                int dime = money / 10;
-                money %= 10;
+//                //10센트 = 1다임
+//                int dime = money / 10;
+//                money %= 10;
 
-                //5센트 = 1니켈
-                int nickel = money / 5;
-                money %= 5;
+//                //5센트 = 1니켈
+//                int nickel = money / 5;
+//                money %= 5;
 
-                //1센티 = 1페니
-                int penny = money / 1;
+//                //1센티 = 1페니
+//                int penny = money / 1;
 
-                Console.WriteLine($"{quarter} {dime} {nickel} {penny}");
-            }
-        }
-    }
-}
+//                Console.WriteLine($"{quarter} {dime} {nickel} {penny}");
+//            }
+//        }
+//    }
+//}
