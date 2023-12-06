0345a3611545ccf489c91025b94a7708d0b01f49
원동현 <ehdgus5500@gmail.com>
2023-12-06 22:44:03 +0900

    백준 10809번

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/10809_FindingAlphabet.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _10809_FindingAlphabet
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<int> result = Enumerable.Repeat(-1, 'z' - 'a' + 1).ToList();
+
+//            string input = Console.ReadLine();
+//            for(int i = 0; i < input.Length; i++) 
+//            {
+//                if (result[input[i] - 'a'] == -1)
+//                {
+//                    result[input[i] - 'a'] = i;
+//                }
+//            }
+
+//            Console.WriteLine(String.Join(' ', result));
+//        }
+//    }
+//}
