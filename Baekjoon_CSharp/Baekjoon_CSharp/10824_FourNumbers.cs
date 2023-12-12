8c82ee4a6be6827c97068d99f4fd86e1679b289c
원동현 <ehdgus5500@gmail.com>
2023-12-12 14:39:38 +0900

    백준 두문제

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/10824_FourNumbers.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _10824_FourNumbers
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<string> inputStrings = Console.ReadLine().Split().ToList();
+
+//            string n1 = inputStrings[0] + inputStrings[1];
+//            string n2 = inputStrings[2] + inputStrings[3];
+
+//            Console.WriteLine(long.Parse(n1) + long.Parse(n2));
+//        }
+//    }
+//}
---
+++ Baekjoon_CSharp/Baekjoon_CSharp/17496_StarFruits.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _17496_StarFruits
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<int> intInputList = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+//            int summerDays = intInputList[0];
+//            int growDays = intInputList[1];
+//            int growSlots = intInputList[2];
+//            int price = intInputList[3];
+
+//            int growCount = summerDays / growDays;
+//            growCount -= (summerDays % growDays) == 0 ? 1 : 0;
+
+//            int totalPrice = growCount * growSlots * price;
+//            Console.WriteLine(totalPrice);
+//        }
+//    }
+//}
