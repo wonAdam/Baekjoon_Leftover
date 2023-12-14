690dc90017a07655510d3eb06dd03b065c1f507d
원동현 <ehdgus5500@gmail.com>
2023-12-14 17:54:04 +0900

    백준 1547번

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/1547_Ball.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _1547_Ball
+//    {
+//        static void Main(string[] args)
+//        {
+//            int m = int.Parse(Console.ReadLine());
+
+//            int ball = 1;
+//            for(int i = 0; i < m; i++)
+//            {
+//                List<int> inputIntList = Console.ReadLine().Split().Select(int.Parse).ToList();
+//                if (inputIntList[0] == ball)
+//                {
+//                    ball = inputIntList[1];
+//                }
+//                else if (inputIntList[1] == ball)
+//                {
+//                    ball = inputIntList[0];
+//                }
+//            }
+
+//            Console.WriteLine(ball);
+//        }
+//    }
+//}
