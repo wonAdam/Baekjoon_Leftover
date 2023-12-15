df8d14ace8956c380248d55accbda63c0b0f0f21
원동현 <ehdgus5500@gmail.com>
2023-12-15 14:16:03 +0900

    백준 1.5문제

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/14720_MilkFestival.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _14720_MilkFestival
+//    {
+//        static void Main(string[] args)
+//        {
+//            int storeCount = int.Parse(Console.ReadLine());
+//            List<int> stores = Console.ReadLine().Split().Select(int.Parse).ToList();
+//            int wantToDrink = 0;
+
+//            int didDrink = 0;
+
+//            for(int currentStoreIndex = 0; currentStoreIndex < storeCount; currentStoreIndex++)
+//            {
+
+//            }
+//        }
+//    }
+//}
---
+++ Baekjoon_CSharp/Baekjoon_CSharp/20499_DariusGosuHasu.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _20499_DariusGosuHasu
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<int> intInputList = Console.ReadLine().Split('/').Select(int.Parse).ToList();
+//            int k = intInputList[0];
+//            int d = intInputList[1];
+//            int a = intInputList[2];
+
+//            if(k + a < d || d == 0)
+//            {
+//                Console.WriteLine("hasu");
+//            }
+//            else
+//            {
+//                Console.WriteLine("gosu");
+//            }
+//        }
+//    }
+//}
