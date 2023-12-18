e76e64169a1b63096f25e5a241b751887bc43d5f
원동현 <ehdgus5500@gmail.com>
2023-12-18 17:10:33 +0900

    백준 두문제

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/1152_TheNumberOfWords.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _1152_TheNumberOfWords
+//    {
+//        static void Main(string[] args)
+//        {
+//            string inputString = Console.ReadLine();
+//            inputString = inputString.Trim();
+//            if (inputString == "")
+//                Console.WriteLine(0);
+//            else
+//                Console.WriteLine(inputString.Split(' ').Length);
+//        }
+//    }
+//}
---
+++ Baekjoon_CSharp/Baekjoon_CSharp/2875_ContestOrInternship.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _2875_ContestOrInternship
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<int> inputIntList = Console.ReadLine().Split().Select(int.Parse).ToList();
+//            int girl = inputIntList[0];
+//            int boy = inputIntList[1];
+//            int intern = inputIntList[2];
+
+//            int team = 0;
+
+//            while(girl + boy - 3 >= intern &&
+//                    girl >= 2 && boy >= 1)
+//            {
+//                girl -= 2;
+//                boy -= 1;
+//                team++;
+//            }
+
+//            Console.WriteLine(team);
+//        }
+//    }
+//}
