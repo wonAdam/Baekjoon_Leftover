4195356c77aa215388d0cd4e3f8987151baef066
원동현 <ehdgus5500@gmail.com>
2023-12-22 16:31:02 +0900

    백준 25191

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/25191_ChickenDance.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _25191_ChickenDance
+//    {
+//        static void Main(string[] args)
+//        {
+//            int chicken = int.Parse(Console.ReadLine());
+//            List<int> inputIntList = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+//            int coke = inputIntList[0];
+//            int beer = inputIntList[1];
+
+//            int maxCanEat = coke / 2 + beer;
+
+//            Console.WriteLine(Math.Min(maxCanEat, chicken));
+//        }
+//    }
+//}
