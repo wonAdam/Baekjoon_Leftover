465bdfce395600c226ad5415e8ec0c783e678ca5
원동현 <ehdgus5500@gmail.com>
2023-12-26 12:10:49 +0900

    백준 두문제

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/24723_GreenTower.cs
@@ -0,0 +1,15 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _24723_GreenTower
+//    {
+//        static void Main(string[] args)
+//        {
+//            int input = int.Parse(Console.ReadLine());
+//            Console.WriteLine(Math.Pow(2, input));
+//        }
+//    }
+//}
---
+++ Baekjoon_CSharp/Baekjoon_CSharp/25238_IgnoreDefenseRate.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _25238_IgnoreDefenseRate
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<int> inputIntList = Console.ReadLine().Split().Select(int.Parse).ToList();
+//            int monsterDefense = inputIntList[0];
+//            int ignoreDefenseRate = inputIntList[1];
+
+//            decimal notIgnoreDefenseRate = 1.0M - Math.Clamp(ignoreDefenseRate / 100.0M, 0.0M, 1.0M);
+//            decimal finalDefenseRate = monsterDefense * notIgnoreDefenseRate;
+
+//            if (finalDefenseRate < 100.0M)
+//            {
+//                Console.WriteLine(1);
+//            }
+//            else
+//            {
+//                Console.WriteLine(0);
+//            }
+//        }
+//    }
+//}
