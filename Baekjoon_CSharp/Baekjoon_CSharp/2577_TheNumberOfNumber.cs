d8a2ea8fd9119373a02c3df903edca435eccf0fc
원동현 <ehdgus5500@gmail.com>
2023-12-28 00:26:25 +0900

    백준 2577번

---
+++ Baekjoon_CSharp/Baekjoon_CSharp/2577_TheNumberOfNumber.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Baekjoon_CSharp
+//{
+//    internal class _2577_TheNumberOfNumber
+//    {
+//        static void Main(string[] args)
+//        {
+//            int a = int.Parse(Console.ReadLine());
+//            int b = int.Parse(Console.ReadLine());
+//            int c = int.Parse(Console.ReadLine());
+
+//            int mul = a * b * c;
+
+//            List<int> result = new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+//            while (mul > 0)
+//            {
+//                result[mul % 10]++;
+//                mul /= 10;
+//            }
+
+//            foreach (int i in result)
+//            {
+//                Console.WriteLine(i);
+//            }
+//        }
+//    }
+//}
